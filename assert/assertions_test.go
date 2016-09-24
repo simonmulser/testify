99b28aed8c78d376bd7a08353b5ee38069cab9b1
Ernesto Jiménez <me@ernesto-jimenez.com>
2016-09-24 18:22:00 +0100

    Fix vet warnings and go generate to update docs

--- assert/assertions_test.go
+++ assert/assertions_test.go
@@ -555,7 +555,7 @@ func TestNoError(t *testing.T) {
 	}()
 
 	if err == nil { // err is not nil here!
-		t.Errorf("Error should be nil due to empty interface", err)
+		t.Errorf("Error should be nil due to empty interface: %s", err)
 	}
 
 	False(t, NoError(mockT, err), "NoError should fail with empty error interface")
@@ -589,7 +589,7 @@ func TestError(t *testing.T) {
 	}()
 
 	if err == nil { // err is not nil here!
-		t.Errorf("Error should be nil due to empty interface", err)
+		t.Errorf("Error should be nil due to empty interface: %s", err)
 	}
 
 	True(t, Error(mockT, err), "Error should pass with empty error interface")
